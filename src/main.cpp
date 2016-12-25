f63a6b5f855647b307238290a15a50635a4a7e73
DaikiMaekawa <daikimaekawa29@gmail.com>
2016-12-25 19:07:01 +0900

    add test func

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int main() {
     sleep(1);
     client.send("vim_del_current_line");
     client.send("vim_list_runtime_paths"); 
+    client.send("vim_set_current_line", "testhogefuga");
 
     for(;;) {}
 
