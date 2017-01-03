3433c4acad284a031bb529137a3f41ee203b7fd7
DaikiMaekawa <daikimaekawa29@gmail.com>
2017-01-04 00:19:12 +0900

    define jinja template

---
+++ templates/api.hpp
@@ -0,0 +1,29 @@
+// Auto generated
+
+#ifndef NEOVIM_CPP__API_HPP_
+#define NEOVIM_CPP__API_HPP_
+
+#include "neovim.hpp"
+
+class Nvim {
+    using Window = int64_t;
+    using Buffer = int64_t;
+    using Tabpage = int64_t;
+    using Integer = int64_t;
+    using Boolean = bool;
+    using String = std::string;
+    using Object = boost::any;
+
+public:
+{% for func in functions%}
+    {{func.return}} {{func.name}} ({% for arg in func.args %}{{arg.type}} {{arg.name}}{% endfor %}) { 
+        return client.send("{{func.name}}", {% for arg in func.args %}{{arg.name}}{% endfor %}); 
+    }
+{% endfor %}
+
+private:
+    NeoVim client;
+
+};
+
+#endif //NEOVIM_CPP__API_HPP_
