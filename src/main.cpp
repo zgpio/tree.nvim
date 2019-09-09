77cf5668c31f1444fa781875ca8d176c74d2fb1a
Hack <zgpio@qq.com>
2019-09-09 19:41:55 +0800

    format

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,61 @@
 #include "nvim.hpp"
-#include <iostream>
-#include <vector>
-#include <string>
 #include <boost/filesystem.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
 using std::cout;
 using namespace boost::filesystem;
 #define BOOST_VARIANT_USE_RELAXED_GET_BY_DEFAULT
 
 void display(nvim::Nvim &nvim, path &p)
 {
-  try
-  {
-    if (exists(p))
-    {
-      if (is_regular_file(p))
-        cout << p << " size is " << file_size(p) << '\n';
+    try {
+        if (exists(p)) {
+            if (is_regular_file(p))
+                cout << p << " size is " << file_size(p) << '\n';
 
-      else if (is_directory(p))
-      {
-        cout << p << " is a directory containing:\n";
+            else if (is_directory(p)) {
+                cout << p << " is a directory containing:\n";
 
-        std::vector<std::string> lines;
-        for (directory_entry& x : directory_iterator(p))
-        {
-          // cout << "    " << x.path() << '\n';
-          lines.push_back(x.path().generic_string());
+                std::vector<std::string> lines;
+                for (directory_entry &x : directory_iterator(p)) {
+                    // cout << "    " << x.path() << '\n';
+                    lines.push_back(x.path().generic_string());
+                }
+                nvim.nvim_buf_set_lines(0, 0, -1, true, lines);
+            }
+            else
+                cout << p
+                     << " exists, but is not a regular file or directory\n";
         }
-          nvim.nvim_buf_set_lines (0, 0, -1, true, lines);
-      }
-      else
-        cout << p << " exists, but is not a regular file or directory\n";
+        else
+            cout << p << " does not exist\n";
     }
-    else
-      cout << p << " does not exist\n";
-  }
 
-  catch (const filesystem_error& ex)
-  {
-    cout << ex.what() << '\n';
-  }
+    catch (const filesystem_error &ex) {
+        cout << ex.what() << '\n';
+    }
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
     nvim::Nvim nvim;
     nvim.connect_tcp("localhost", "6666");
     nvim.nvim_eval("\( 3 + 2 \) \* 4");
-    std::cout << "get_current_line = " << nvim.nvim_get_current_line() << std::endl;
+    std::cout << "get_current_line = " << nvim.nvim_get_current_line()
+              << std::endl;
     nvim.vim_set_current_line("testhogefuga");
 
-
-    if (argc < 2)
-    {
+    if (argc < 2) {
         cout << "Usage: tut3 path\n";
         return 1;
     }
 
-    path p (argv[1]);
+    path p(argv[1]);
     display(nvim, p);
 
-    for(;;) {}
+    for (;;) {
+    }
 
     return 0;
 }
