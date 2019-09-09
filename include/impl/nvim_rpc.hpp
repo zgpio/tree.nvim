5f789dc2996e0762e5308c586379984c195e516b
Hack <zgpio@qq.com>
2019-09-09 09:10:20 +0800

    format

--- include/impl/nvim_rpc.hpp
+++ include/impl/nvim_rpc.hpp
@@ -7,7 +7,7 @@ namespace detail {
     Packer& pack(Packer& pk, const T& t) {
           return pk << t;
     }
-    
+
     template<class T1, class T2, class...T3>
     Packer& pack(Packer& pk, const T1 &t1, const T2 &t2, const T3 &...t3) {
           return pack(pack(pk, t1), t2, t3...);
@@ -23,7 +23,7 @@ template<typename T, typename...U>
 void NvimRPC::call(const std::string &method, T& res, const U&...u) {
     Object v = do_call(method, u...);
     std::cout << "T NvimRPC::call" << std::endl;
-    
+
     res = boost::get<T>(v);
 }
 
@@ -31,7 +31,7 @@ template<typename...U>
 void NvimRPC::call(const std::string &method, Integer& res, const U& ...u) {
     Object v = do_call(method, u...);
     std::cout << "Integer NvimRPC::call" << std::endl;
-    
+
     // int64_t is only for negative integer.
     if(v.is_int64_t())       res = v.as_int64_t();
     else if(v.is_uint64_t()) res = v.as_uint64_t();
@@ -58,25 +58,25 @@ Object NvimRPC::do_call(const std::string &method, const U&...u) {
     pk.pack_array(4) << (uint64_t)REQUEST
                      << msgid_++
                      << method;
-    
+
     pk.pack_array(sizeof...(u));
     detail::pack(pk, u...);
-    
+
     msgpack::object_handle oh = msgpack::unpack(sbuf.data(), sbuf.size());
 
     msgpack::object deserialized = oh.get();
 
     std::cout << "sbuf = " << deserialized << std::endl;
-    
+
     socket_.write(sbuf.data(), sbuf.size(), 5);
-    
+
     msgpack::unpacker unpacker;
     unpacker.reserve_buffer(32*1024ul);
-    
+
     size_t rlen = socket_.read(unpacker.buffer(), unpacker.buffer_capacity(), 5);
     msgpack::unpacked result;
     unpacker.buffer_consumed(rlen);
-    
+
     /*
     while(unpacker.next(result)) {
         const msgpack::object &obj = result.get();
@@ -84,7 +84,7 @@ Object NvimRPC::do_call(const std::string &method, const U&...u) {
         result.zone().reset();
     }
     */
-    
+
     //TODO: full-state response handler should be implemented
     unpacker.next(result);
     const msgpack::object &obj = result.get();
