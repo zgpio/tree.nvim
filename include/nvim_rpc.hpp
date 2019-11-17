059d5bb4697daf87fba32ff1451fb965dba43051
Hack <zgpio@qq.com>
2019-11-17 22:31:47 +0800

    add msg dispatch

--- include/nvim_rpc.hpp
+++ include/nvim_rpc.hpp
@@ -52,10 +52,54 @@ public:
 
     // TODO: 临时
     void eventloop() {
+        using std::cerr;
+        using std::string;
         std::cout << "eventloop started" << std::endl;
         while(true) {
             msgpack::unpacked result = socket_.read2(10);
             msgpack::object obj(result.get());
+            if (obj.type != msgpack::type::ARRAY) {
+                cerr << "Received Invalid msgpack: not an array";
+                return;
+            }
+
+            uint64_t type = obj.via.array.ptr[0].via.u64;
+
+            switch(type) {
+            case 0:
+                if (obj.via.array.ptr[1].type != msgpack::type::POSITIVE_INTEGER) {
+                    cerr << "Received Invalid request: msg id MUST be a positive integer";
+                    return;
+                }
+                if (obj.via.array.ptr[2].type != msgpack::type::BIN &&
+                        obj.via.array.ptr[2].type != msgpack::type::STR) {
+                    cerr << "Received Invalid request: method MUST be a String" << obj.via.array.ptr[2];
+                    return;
+                }
+                if (obj.via.array.ptr[3].type != msgpack::type::ARRAY) {
+                    cerr << "Invalid request: arguments MUST be an array";
+                    return;
+                }
+                break;
+            case 1:
+                if (obj.via.array.ptr[1].type != msgpack::type::POSITIVE_INTEGER) {
+                    cerr << "Received Invalid response: msg id MUST be a positive integer";
+                    return;
+                }
+                break;
+            case 2:
+                {
+                    Object methodName;
+                    // [type(2), method, params]
+                    msgpack::type::tuple<int64_t, Object, Object> msg;
+                    obj.convert(msg);
+                    std::cout << msg.get<1>().as_string() << std::endl;
+                }
+
+                break;
+            default:
+                cerr << "Unsupported msg type" << type;
+            }
             std::cout << "res = " << obj << std::endl;
         }
     }
